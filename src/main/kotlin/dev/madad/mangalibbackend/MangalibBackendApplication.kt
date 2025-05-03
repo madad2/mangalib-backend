27b229cf861fb6505e5541ef6ef9780b0f51da64
madad2 <alexander.laputko@ya.ru>
2025-05-03 23:55:55 +0300

    Initialize Spring Boot Kotlin project with Gradle setup
    Added a new Spring Boot backend project named "mangalib-backend".

---
+++ src/main/kotlin/dev/madad/mangalibbackend/MangalibBackendApplication.kt
@@ -0,0 +1,11 @@
+package dev.madad.mangalibbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MangalibBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<MangalibBackendApplication>(*args)
+}
---
+++ src/test/kotlin/dev/madad/mangalibbackend/MangalibBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.madad.mangalibbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MangalibBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
